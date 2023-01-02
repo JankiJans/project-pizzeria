a355600e9bc7ffe0891ff3fb2d24a14d46bb3dee
Jan Kędzierski <jkedzierski087@gmail.com>
2023-01-02 17:35:26 +0100

    add function initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -60,6 +60,7 @@
       thisProduct.data = data; //odnośnik do opisu produktu który znajduję się pod id czyli class, name, price itd
 
       thisProduct.renderInMenu();
+      initAccordion();
 
       console.log('new Product:', thisProduct);
     }
@@ -83,6 +84,40 @@
       /* add element to menu */
       
       menuContainer.appendChild(thisProduct.element);
+
+    }
+
+    initAccordion(){
+      const thisProduct = this;
+
+      /* find the clickable trigger (the element that should react to clicking) */
+
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); // stała która szuka konkretnego elemntu z `thisProduct.element` znaleziona została w stałej `select` poźniej w menuProduct` i na końcu `clikable`
+
+      /* START: add event listener to clickable trigger on event click */
+
+      clickableTrigger.addEventListener('click', function(event){
+
+      /* prevent default action for event */
+
+      event.preventDefault(); //to pozwala zapobiec domyślnej akcji dla zdarzenia np. blokuje przekierowanie na inną stronę.
+
+      /* find active product (product that has active class) */
+
+      const activeProduct = thisProduct.element.querySelector(classNames.menuProduct.wrapperActive);
+
+      /* if there is active product and it's not thisProduct.element, remove class active from it */
+
+      if(activeProduct && activeProduct !== thisProduct.element){
+
+        activeProduct.classList.remove('actiive');
+      }
+
+      /* toggle active class on thisProduct.element */
+
+      thisProduct.element.classList.toggle('active');
+
+      });
 
     }
   }
