638ef391089514c513fd941726b8c0244b863bb8
Jan Kędzierski <jkedzierski087@gmail.com>
2023-01-11 16:25:18 +0100

    add class CartProduct and getElemnts, initAmountWiget functions

--- src/js/script.js
+++ src/js/script.js
@@ -498,7 +498,61 @@
 
       console.log('adding product', menuProduct);
 
+      //thisCart.products.push(menuProduct); // pokazuje co jest w koszyku
+      console.log('thisCart.products', thisCart.products);
+
+      thisCart.products.push(new CartProduct(menuProduct, generatedDOM));
+
     }
+  }
+
+  class CartProduct {
+    constructor(menuProduct, element){
+      const thisCartProduct = this;
+
+      thisCartProduct.id = menuProduct.id,
+      thisCartProduct.name = menuProduct.name,
+      thisCartProduct.amount = menuProduct.amount,
+      thisCartProduct.priceSingle = menuProduct.priceSingle,
+      thisCartProduct.price = menuProduct.priceSingle;
+      thisCartProduct.params = menuProduct.params;
+
+      thisCartProduct.getElements(element);
+      thisCartProduct.initAmountWidget();
+
+
+      console.log('thisCartProduct:', thisCartProduct);
+
+    }
+
+    getElements(element){
+      const thisCartProduct = this;
+
+      thisCartProduct.dom = {
+        wrapper: element,
+        amountWidgetElem: element.querySelector(select.cartProduct.amountWidget),
+        price: element.querySelector(select.cartProduct.price),
+        edit: element.querySelector(select.cartProduct.edit),
+        remove: element.querySelector(select.cartProduct.remove),
+      };
+
+    }
+
+    initAmountWidget(){
+      const thisCartProduct = this;
+
+      thisCartProduct.amountWidget = new AmountWidget(thisCartProduct.dom.amountWidgetElem);
+
+      thisCartProduct.dom.amountWidgetElem.addEventListener('updated', function(){
+
+        thisCartProduct.amount = thisCartProduct.amountWidget.value;
+        thisCartProduct.price = thisCartProduct.amount * thisCartProduct.priceSingle;
+
+        thisCartProduct.dom.price.innerHTML = thisCartProduct.price;
+        
+      });
+    }
+    
   }
   const app = {
     initMenu: function(){
